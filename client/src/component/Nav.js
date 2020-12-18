150ac3a463e1d9954305804508f47454b929054b
Schwyn Francis <schwynf@gmail.com>
2020-12-18 11:50:42 -0700

    css

--- client/src/component/Nav.js
+++ client/src/component/Nav.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios"
+import * as Scroll from 'react-scroll';
+import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 import "./Nav.css"
 
 const Nav = () => {
@@ -13,13 +15,19 @@ const Nav = () => {
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a className="nav-link" href="#secondRow">Story</a>
+                        <Link activeClass="active" to="our-story" spy={true} smooth={true} offset={-50} duration={600}>
+                        <a className="nav-link">Story</a> 
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#thirdRow">Party</a>
+                    <Link activeClass="active" to="wedding-party" spy={true} smooth={true} offset={-50} duration={600}>
+                        <a className="nav-link">Party</a> 
+                        </Link>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="/Travel">Travel</a>
+                    <Link activeClass="active" to="scoll" spy={true} smooth={true} offset={-50} duration={600}>
+                        <a className="nav-link">Travel</a> 
+                        </Link>
                     </li>
                     <li className="nav-item">
                     <a className="nav-link" href="" data-toggle="modal" data-target="#exampleModal">Contact</a>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -32,18 +32,15 @@ const Home = () => {
 
     return (
         <>
-            <Confetti className="mt-2"></Confetti>
+            <Confetti></Confetti>
             <Nav></Nav>
             <div className="container">
                 {/* first row */}
-                <div className="row" id="firstRow">
+                <div className="row mt-5" id="firstRow">
                     <div className="col-md-1 p-0 erase" id="firstDiv">
                     </div>
                     <div className="col-12 col-md-10 p-0" id="gradient">
                         <HomeContent></HomeContent>
-                        <Link activeClass="active" to="scoll" spy={true} smooth={true} offset={-50} duration={600}>
-          Test 1
-        </Link>
                     </div>
                     <div className="col-md-1 p-0 erase" id="please">
                     </div>
@@ -55,7 +52,7 @@ const Home = () => {
                     <div className="col-12 col-md-10 p-0" id="gradient">
                         <div className="text-center text-light">
                             <p style={{ fontSize: "3rem", fontFamily: 'Sansita Swashed' }}>
-                                <cite>Our Story</cite>
+                                <cite id="our-story">Our Story</cite>
                             </p>
                             <div id="canvas" className="text-light">
                                 Nisi scelerisque eu ultrices vitae auctor. Pellentesque massa placerat duis ultricies lacus sed turpis tincidunt. Libero volutpat sed cras ornare arcu dui vivamus. Dignissim cras tincidunt lobortis feugiat vivamus at. Neque vitae tempus quam pellentesque nec nam aliquam. Orci ac auctor augue mauris. Vel facilisis volutpat est velit egestas dui. Tempus quam pellentesque nec nam aliquam sem et tortor.
@@ -75,7 +72,7 @@ const Home = () => {
                     <div className="col-12 col-md-10 p-0" id="gradient">
                         <div className="text-center text-danger" style={{ fontFamily: 'Sansita Swashed' }}>
                             <p style={{ fontSize: "3rem" }}>
-                                <cite id="scoll">Wedding Party</cite>
+                                <cite id="wedding-party">Wedding Party</cite>
                             </p>
                         </div>
                     </div>
