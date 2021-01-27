d5e9ece4353ec0e46821d84f216e5dc3744f9d95
Schwyn Francis <schwynf@gmail.com>
2021-01-26 19:46:16 -0700

    slide

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -61,7 +61,7 @@ const Home = () => {
                 {/* {Join Us Content} */}
                 <div className="row" id="when-content">
                     <div className="col-md-6 when-content-pink">
-                        <Slide left duration="1500">
+                        
                         <div className="text-center">JOIN US!</div>
                         <br></br>
                         <div id="border"></div>
@@ -71,24 +71,24 @@ const Home = () => {
                         </h1>
                         <h1 className="when-font text-center"> &Where</h1>
 
-                        </Slide>
+                       
                     </div>
                     <div className="col-md-6 when-content-beige time-font text-center tt">
-                        <Slide right duration="2000">
+                     
 
                         <div>FRIDAY, 08.09.2020 <br></br>E Jomax Rd, Phoenix</div>
                         <div className="" id="3"><span className="time">3 pm</span> Arrival</div>
                         <div className="mt-3" id="4"><span className="time">4 pm</span> Ceremony</div>
                         <div className=" mt-3 time-bgcolor" id="5"><span className="time">5 pm</span> Celebration</div>
-                        </Slide>
+                       
                     </div>
                 </div>
                 {/* Registry/RSVP */}
                 <div className="row text-center" id="registry-rvsp-content">
                     <div className="col-md-6 registry col-sm-offset-6">
-                        {rsvp ? (<div><Slide left><h1 className="text-center registry-h1">
+                        {rsvp ? (<div><h1 className="text-center registry-h1">
                             Schwyn & Kalee Wedding</h1>
-                            <button className="btn btn-warning mt-2 but" type="submit" onClick={checkRSVP}>RVSP Now!</button></Slide></div>) : (<div><div>Thanks!!!!!!!</div>
+                            <button className="btn btn-warning mt-2 but" type="submit" onClick={checkRSVP}>RVSP Now!</button></div>) : (<div><div>Thanks!!!!!!!</div>
                                 <br></br>
                                 <br></br>
                                 <input className="form-control" type="text" placeholder="Name"></input>
@@ -100,14 +100,14 @@ const Home = () => {
                                 <button className="btn btn-warning mt-2" type="submit" onClick={checkRSVP}>Submit!</button></div>)}
                     </div>
                     <div className="col-md-6 rvsp text-center">
-                        <Slide right>
+                      
 
                         <h1 className="text-center registry-h1">
                             Gift<br></br>
                             Registry
                         </h1>
                         <button className="btn btn-warning registry-button but" type="submit" onClick={amazon}>Wedding Registry</button>
-                        </Slide>
+                     
                     </div>
                 </div>
                 <div className="row wedding-pic-content">
